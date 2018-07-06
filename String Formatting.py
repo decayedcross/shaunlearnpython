02e47b39ac1faf2549d5ad22b5450a722a1c6d20
Shaun <decayedcross@gmail.com>
2018-07-06 01:40:47 -0700

    new lesson

---
+++ String Formatting.py
@@ -0,0 +1,9 @@
+#You will need to write a format string which prints out the data using the following syntax: Hello John Doe. Your current balance is $53.44.
+
+
+
+
+print("{0} {1} {2}. Your current balance is {3}".format("Hello", "John", "Doe", 53.44))
+
+
+
