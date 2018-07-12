942e6392daa127d94aa5c871879da175695a3e95
Shaun <decayedcross@gmail.com>
2018-07-12 01:30:06 -0700

    updated into to class -not working

--- variables_and_types.py
+++ variables_and_types.py
@@ -1,11 +1,26 @@
+class VariablesAndTypes:
+	def __init__ (self, mystring, myfloat, myint):
+		self.mystring = mystring
+		self.myfloat = myfloat
+		self.myint = myint
+		return 	
+
+
 mystring = "hello"
 myfloat = 10.0
-myint = 20
+myint =  20
+
+fullstring = VariablesAndTypes("hello", 10.0, 20)
+
+
 
 # testing code
 if mystring == "hello":
-    print("String: %s" % mystring)
+    print("String: " + mystring)
 if isinstance(myfloat, float) and myfloat == 10.0:
-    print("Float: %f" % myfloat)
+    print("Float: " + myfloat)
 if isinstance(myint, int) and myint == 20:
-    print("Integer: %d" % myint)
+    print("Integer: " + myint)
+
+
+   
