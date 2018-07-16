ab142c480b41c403a2ac04eb25b7e02f7131a35e
decayedcross <decayedcross@gmail.com>
2018-07-16 04:40:08 -0700

    i still suck but I made a little progress on this one

--- Lists.py
+++ Lists.py
@@ -3,22 +3,38 @@
 #You will also have to fill in the variable second_name with the second name in the names list, using the brackets operator []. Note that the index is zero-based, so if you want to access the second item in the list, its index will be 1.
 
 
-numbers = []
-strings = []
-names = ["John", "Eric", "Jessica"]
+class List:
+	def __init__(self):
+		pass	
+
+	#def app(self, string, x):		
+		#self.string = string
+	#	return self.string.append(x)
+
+	def names(self, names):
+		self.names = names
+		self.message = "The second name on the names list is {}"	
+		return self.message.format(names)
+
+
+names = [
+	"John", "Eric", "Jessica"
+]	
 
 # write your code here
-numbers.append(1)
-numbers.append(2)
-numbers.append(3)
 
-second_name = names[1]
+l = List()
 
-strings.append("hello")
-strings.append("world")
+#numbers = l.app('num',1)
 
+eric = l.names("Eric")
+
+print(eric)
 
 # this code should write out the filled arrays and the second name in the names list (Eric).
-print(numbers)
-print(strings)
-print("The second name on the names list is %s" % second_name)
+#print(numbers)
+#print(strings)
+#print("The second name on the names list is %s" % second_name)
+
+
+
