8f523b1bd734a8c89c3e46fb5224794d26441b03
decayedcross <decayedcross@gmail.com>
2018-07-18 13:20:54 -0700

    new lesson learned

--- basic_operators.py
+++ basic_operators.py
@@ -1,20 +1,24 @@
 #The target of this exercise is to create two lists called x_list and y_list, which contain 10 instances of the variables x and y, respectively. 
 #You are also required to create a list called big_list, which contains the variables x and y, 10 times each, by concatenating the two lists you have created.
 
-x = object()
-y = object()
+
+class Operations:
+	def __init__(self):
+		pass
+
+	def x(self, x_number):		
+		return [object()] * x_number
+
+	def add_list(self, a_list, b_list):
+		return a_list + b_list
 
 # TODO: change this code
-x_list = [x] * 10
-y_list = [y] * 10
-big_list = x_list + y_list
 
-print("x_list contains %d objects" % len(x_list))
-print("y_list contains %d objects" % len(y_list))
-print("big_list contains %d objects" % len(big_list))
+basic = Operations()
 
-# testing code
-if x_list.count(x) == 10 and y_list.count(y) == 10:
-    print("Almost there...")
-if big_list.count(x) == 10 and big_list.count(y) == 10:
-    print("Great!")
+x_list = basic.x(10)
+y_list = basic.x(10)
+
+big_list = basic.add_list(x_list, y_list)
+
+print(x_list, "\n \n",  y_list, "\n \n", big_list)
