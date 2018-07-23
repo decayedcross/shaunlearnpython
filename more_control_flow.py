45e43f111353566045cf99a9ebd7103e457f24b0
decayedcross <decayedcross@gmail.com>
2018-07-23 15:14:10 -0700

    4.1 notes

--- more_control_flow.py
+++ more_control_flow.py
@@ -1,4 +1,10 @@
 #4.1
+### NOTES ###
+
+# if - statement is used for conditional execution
+# elif - not required in if statement, can be more than 1, short for else if.
+# else - not required in if statement
+
 x = 420
 
 if x == 420:
@@ -11,7 +17,12 @@ else:
 	print('this is a SHIT number, GTFO')
 
 
+
 #4.2
+### NOTES ###
+
+#for - loops through the content of a given list or string in numerical order, starting with 0
+
 four_point_two = ['lesson', 'four', 'point', 'two']
 
 for length in four_point_two:
@@ -52,3 +63,11 @@ for blaze in range(415, 423):
 		print(blaze, 'blaze it')
 		continue
 	print(blaze, 'is a usless number')
+
+
+
+	Python’s for statement iterates over the items of any sequence (a list or a string), in the order that they appear in the sequence. For example (no pun intended):
+
+If you need to modify the sequence you are iterating over while inside the loop (for example to duplicate selected items), it is recommended that you first make a copy. 
+Iterating over a sequence does not implicitly make a copy. The slice notation makes this especially convenient:
+
