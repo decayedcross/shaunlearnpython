4279251f08ac932b6a3d4750b817628375a42fa8
decayedcross <decayedcross@gmail.com>
2018-07-24 04:54:34 -0700

    4.6 i dont get everyting going on here and its frustrating

--- more_control_flow.py
+++ more_control_flow.py
@@ -77,4 +77,65 @@ for blaze in range(415, 423):
 		continue
 	print(blaze, 'is a usless number')
 
+#4.5
 
+#pass - skip
+
+
+#4.6
+def fib(n):    # write Fibonacci series up to n
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+# Now call the function we just defined:
+fib(2000)
+
+def blaze(a, b):
+	for blaze in range(a, b):
+		if blaze == 421:
+			print(blaze,'we\'ve gone to far')
+			break
+		else:
+			if blaze == 420:
+				print(blaze, 'blaze it')
+				continue
+			print(blaze, 'is a usless number')
+
+c = blaze(415, 423)
+
+
+
+
+
+
+#def introduces a function and must allways be followed by the function name and parenthesized with any parameters inside the paranthasieze 
+
+#The first statement of the function body can optionally be a string literal; this string literal is the function’s documentation string, or docstring. 
+
+
+#The execution of a function introduces a new symbol table used for the local variables of the function. 
+#More precisely, all variable assignments in a function store the value in the local symbol table; whereas variable references first look in the local symbol table, 
+#then in the local symbol tables of enclosing functions, then in the global symbol table, and finally in the table of built-in names. 
+#Thus, global variables cannot be directly assigned a value within a function (unless named in a global statement), although they may be referenced.
+
+#The actual parameters (arguments) to a function call are introduced in the local symbol table of the called function when it is called; 
+#thus, arguments are passed using call by value (where the value is always an object reference, not the value of the object). [1] 
+#When a function calls another function, a new local symbol table is created for that call.
+
+#A function definition introduces the function name in the current symbol table.
+# The value of the function name has a type that is recognized by the interpreter as a user-defined function. 
+# This value can be assigned to another name which can then also be used as a function. This serves as a general renaming mechanism:
+
+
+
+#The return statement returns with a value from a function. return without an expression argument returns None. Falling off the end of a function also returns None.
+#The statement result.append(a) calls a method of the list object result. 
+#A method is a function that ‘belongs’ to an object and is named obj.methodname, where obj is some object (this may be an expression),
+# and methodname is the name of a method that is defined by the object’s type. Different types define different methods.
+#  Methods of different types may have the same name without causing ambiguity. (It is possible to define your own object types and methods, using classes, see Classes) 
+#  The method append() shown in the example is defined for list objects; it adds a new element at the end of the list.
+#   In this example it is equivalent to result = result + [a], but more efficient.
